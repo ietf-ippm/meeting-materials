ebc17bd90f53705164809783796248d7565be132
Tommy Pauly <tpauly@apple.com>
2019-03-07 15:33:50 -0800

    Update IETF number

--- ietf104-prague/mkagenda.sh
+++ ietf104-prague/mkagenda.sh
@@ -1,1 +1,1 @@
-pandoc -M "title:IPPM Agenda IETF 103" --standalone --self-contained --css agenda.css agenda.md > agenda.html
+pandoc -M "title:IPPM Agenda IETF 104" --standalone --self-contained --css agenda.css agenda.md > agenda.html
--- ietf104-prague/mkminutes.sh
+++ ietf104-prague/mkminutes.sh
@@ -1,1 +1,1 @@
-pandoc -M "title:IPPM Minutes IETF 103" --standalone --self-contained --css agenda.css minutes.md > minutes.html
+pandoc -M "title:IPPM Minutes IETF 104" --standalone --self-contained --css agenda.css minutes.md > minutes.html
