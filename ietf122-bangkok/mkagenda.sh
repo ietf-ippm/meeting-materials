b8c5833f99b2694c762e4ec131ff651048c356c8
Marcus Ihlar <marcus@ihlar.se>
2025-03-05 17:09:09 +0100

    make agenda

--- ietf122-bangkok/mkagenda.sh
+++ ietf122-bangkok/mkagenda.sh
@@ -1,1 +1,1 @@
-pandoc -M "title:IPPM Agenda IETF 121" --embed-resources --standalone --css agenda.css agenda.md > agenda.html
+pandoc -M "title:IPPM Agenda IETF 122" --embed-resources --standalone --css agenda.css agenda.md > agenda.html
